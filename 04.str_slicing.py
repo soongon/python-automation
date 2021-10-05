b850cd85440f12e9d80b5961d4393878aa347c0c
soongon <soongon@hucloud.co.kr>
2021-10-05 13:45:49 +0900

    initial commit

---
+++ 04.str_slicing.py
@@ -0,0 +1,11 @@
+my_str = 'Hello world!'
+
+# llo 를 슬라이싱
+result = my_str[2:5]
+print(result)
+
+# 슬라이싱의 실제 사례
+original_str = '-hello world-'
+# 양 옆에 대쉬(-)를 제거
+result_str = original_str[1:-1]
+print(result_str)

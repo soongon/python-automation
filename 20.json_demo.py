cce9fa83374359bbea58dafbc45331a10b04fe88
soongon <soongon@hucloud.co.kr>
2021-10-06 16:11:22 +0900

    initial commit

---
+++ 20.json_demo.py
@@ -0,0 +1,14 @@
+url = 'https://api.github.com/users'
+
+# web agent.. requests
+# pip install requests 로 requests 모듈 설치
+import requests
+
+res = requests.get(url)
+
+# user 데이터 중 html_url 의 값을 리스트로 반환
+result_list = []
+for user in res.json():
+    result_list.append(user.get('html_url'))
+
+print(result_list, len(result_list))
